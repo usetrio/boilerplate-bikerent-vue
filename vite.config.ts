1ae9af3d69b8e38b0b6c016ee0392a55decdd6c2
Trio Challenge <challenge@trio.dev>
2022-10-27 20:13:40 -0300

    feat(UI): SASS variables, mixins, functions  and utility classes added to Vue components

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { URL, fileURLToPath } from 'node:url'
 
 import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
+import { extname } from 'node:path'
 import postcssExtend from 'postcss-extend-rule'
 import vue from '@vitejs/plugin-vue'
 
@@ -14,6 +15,26 @@ export default defineConfig({
     }
   },
   css: {
+    preprocessorOptions: {
+      scss: {
+        // NOTE: this option is not documented on Vite, but it is possible to use additionalData as function
+        // This helps to check what files we need to prepend CSS before parsing it. It is very useful for Vue components only.
+        // @see https://github.com/vitejs/vite/blob/177b427b1b0c72b06bddd860d14ff119cb22431f/packages/vite/src/node/plugins/css.ts#L1401-L1408
+        //
+        // WARNING: This may change in vite future versions. Project is using vite@3.1.8
+        additionalData(source: string, filename: string) {
+          let additionalData = ''
+          // Include clean stylesheet to add variables, mixins, functions and helpers to Vue <style> generated CSS.
+          if (extname(filename) === '.vue') {
+            additionalData = `
+              @import '@/assets/scss/component.scss';
+            `
+          }
+
+          return `${additionalData.trim()}\n${source}`
+        }
+      }
+    },
     postcss: {
       // TODO: check nesting
       plugins: [autoprefixer, postcssExtend({ name: 'apply' })]
