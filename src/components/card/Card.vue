1dfbe84b2d76a8e2550e2e40e96149316816ee5f
Trio Challenge <challenge@trio.dev>
2022-10-27 20:35:14 -0300

    feat(UI): common components added
    - card component
    - chip component

---
+++ src/components/card/Card.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Card'
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card__image">
+      <slot name="image" />
+    </div>
+    <div class="card__title">
+      <slot name="title" />
+    </div>
+    <div class="card__content">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  @apply .p-5;
+
+  position: relative;
+  border-radius: $border-radius-xl;
+  border: 1px solid get-theme-color('gray');
+}
+</style>
---
+++ src/components/chip/Chip.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Chip',
+  props: {
+    // TODO: use props
+    size: {
+      type: String,
+      default: 'sm'
+    },
+    color: {
+      type: String,
+      default: 'gray'
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="chip">
+    <div class="chip__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chip {
+  @apply .relative, .items-center, .inline-flex, .max-w-full, .overflow-hidden, .py-2, .px-3;
+
+  line-height: 20px;
+  outline: none;
+  text-decoration: none;
+  vertical-align: middle;
+  background-color: get-theme-color('gray');
+  border-radius: $border-radius-md;
+
+  @include element('content') {
+    @apply .inline-flex, .h-ful, .items-center, .max-w-full;
+  }
+}
+</style>
