fe2c12216c5f09d0d670a27992f2ae1a80b712f6
Trio Challenge <challenge@trio.dev>
2022-10-27 20:36:30 -0300

    feat(core): eslint rule added for vue components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -46,6 +46,7 @@ module.exports = {
     'vue/component-api-style': ['error', ['options']],
     'vue/component-name-in-template-casing': ['error', 'kebab-case', { registeredComponentsOnly: true }],
     'vue/multi-word-component-names': ['error', { ignores: ['Chip', 'Card'] }],
-    'vue/no-unused-components': [productionRule]
+    'vue/no-unused-components': [productionRule],
+    'vue/no-unused-properties': [productionRule, { groups: ['props', 'computed', 'data'] }]
   }
 }
