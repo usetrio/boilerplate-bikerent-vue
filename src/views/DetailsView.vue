493535f2f351e1e0b7106d3ebc9f7f37e9f5a752
Trio Challenge <challenge@trio.dev>
2022-11-04 12:07:45 -0300

    feat: bike details page added (wip)
    - router module added
    - view file created

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'DetailsView'
+})
+</script>
+
+<template>
+  <div class="page page--details"></div>
+</template>
+
+<style lang="scss" scoped>
+.page--details {
+  position: relative;
+}
+</style>
