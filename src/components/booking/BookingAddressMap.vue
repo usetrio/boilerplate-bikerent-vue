0181ba96f16e1e8a04ebca17e365536d4c7064a4
Trio Challenge <challenge@trio.dev>
2022-11-07 10:47:22 -0300

    feat(UI): booking address component added

---
+++ src/components/booking/BookingAddressMap.vue
@@ -0,0 +1,54 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BookingAddressMap',
+  props: {
+    address: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    hasAddress() {
+      return !!this.address
+    }
+  },
+  methods: {
+    getMapUrl(address: string) {
+      return `https://maps.google.com/maps?width=750&height=400&hl=en&q=${decodeURIComponent(
+        address
+      )}&ie=UTF8&iwloc=B&output=embed&t=&z=15`
+    }
+  }
+})
+</script>
+
+<template>
+  <section class="booking-address-map">
+    <template v-if="hasAddress">
+      <iframe
+        :src="getMapUrl(address)"
+        height="400"
+        width="100%"
+        frameBorder="0"
+        scrolling="no"
+        marginHeight="0"
+        marginWidth="0"
+        seamless
+      />
+    </template>
+    <template v-else>
+      <p>No data available</p>
+    </template>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.booking-address-map {
+  position: relative;
+
+  border-radius: $border-radius-lg;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/booking/index.ts
@@ -0,0 +1,1 @@
+export { default as BookingAddressMap } from './BookingAddressMap.vue'
