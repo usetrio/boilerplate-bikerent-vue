9ad7a2eaae17d2c7600a5b00e3b82ce5070a7e7b
Trio Challenge <challenge@trio.dev>
2022-11-07 10:48:50 -0300

    feat: bike view page updates (wip)
    - data integration

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -3,20 +3,31 @@ import { useBikeStore, mapActions, mapState } from '@/core/store'
 import { defineComponent } from 'vue'
 
 import { LoadingSpinner } from '@/components/loading'
-import { BikeImageSelector, BikeSpecs, type BikeSpecsProps } from '@/components/bike'
+import { BikeImageSelector, BikeSpecs, type BikeSpecsProps, BikePrice } from '@/components/bike'
+import { Chip } from '@/components/chip'
+import { PropCurrencyCode } from '@/components/bike/BikePrice.vue'
+import { BookingAddressMap } from '@/components/booking'
 
 export default defineComponent({
   name: 'DetailsView',
   components: {
     LoadingSpinner,
     BikeImageSelector,
-    BikeSpecs
+    BikeSpecs,
+    BikePrice,
+    Chip,
+    BookingAddressMap
   },
   data: () => ({
-    isLoading: false
+    isLoading: false,
+    currency: PropCurrencyCode.EUR,
+    mockAddress: '745 Atlantic Ave, Boston, MA 02111, United States'
   }),
   computed: {
     ...mapState(useBikeStore, ['getBikeById']),
+    hasData() {
+      return !!this.data
+    },
     id() {
       const { id } = this.$route.params || {}
 
@@ -59,12 +70,39 @@ export default defineComponent({
         <loading-spinner />
       </div>
     </template>
+    <template v-else-if="!hasData">
+      <p>No data available</p>
+    </template>
     <template v-else>
       <div class="grid gap-x-6 grid-cols-1">
         <div class="card">
           <bike-image-selector :images="images" class="mb-8" />
 
           <bike-specs :specs="specs" />
+
+          <div class="divider" />
+
+          <article>
+            <h2 class="font-extrabold text-4xl">{{ data!.name }}</h2>
+            <chip color="secondary" size="sm">{{ data!.type }}</chip>
+
+            <p>{{ data!.description }}</p>
+          </article>
+
+          <section class="pricing">
+            <div class="divider" />
+            <div class="flex">
+              <div class="font-semibold">Day</div>
+              <bike-price :price="data!.rate" :currency="currency" rate="daily" class="ml-auto" />
+            </div>
+            <div class="flex">
+              <div class="font-semibold">Week</div>
+              <bike-price :price="data!.rate * 7" :currency="currency" rate="weekly" class="ml-auto" />
+            </div>
+            <div class="divider" />
+          </section>
+
+          <booking-address-map :address="mockAddress" />
         </div>
         <div class="card"></div>
       </div>
