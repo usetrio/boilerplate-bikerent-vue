0ace73703799a47e90d1afc68dbe226ab8673d15
Trio Challenge <challenge@trio.dev>
2022-12-14 22:07:32 -0300

    feat(tests): tests added for BikeCard component

---
+++ src/components/bike/__tests__/BikeCard.spec.ts
@@ -0,0 +1,54 @@
+import { BikeBookmark, BikeCard, BikePrice } from '@/components/bike'
+import { describe, expect, it, vi } from 'vitest'
+
+import { CurrencyCode } from '@/core/config'
+import { IntersectionObserverMock } from '@/core/helpers/tests'
+import { mockedBike } from '@/mocks/'
+import { mount } from '@vue/test-utils'
+import router from '@/router'
+
+describe('BikeCard', () => {
+  vi.stubGlobal('IntersectionObserver', IntersectionObserverMock)
+
+  it('renders correctly', () => {
+    const wrapper = mount(BikeCard, { props: { data: mockedBike }, global: { plugins: [router] } })
+
+    expect(wrapper.text()).toMatchSnapshot()
+  })
+
+  it('displays title', () => {
+    const wrapper = mount(BikeCard, { props: { data: mockedBike, fluid: true }, global: { plugins: [router] } })
+
+    expect(wrapper.find('.bike-card__title').text()).equals(mockedBike.name)
+  })
+
+  it('renders url', () => {
+    const wrapper = mount(BikeCard, { props: { data: mockedBike, fluid: true }, global: { plugins: [router] } })
+    const { fullPath: routeStub } = router.resolve({ name: 'details', params: { id: mockedBike.id } })
+
+    expect(wrapper.findComponent({ name: 'router-link' }).exists()).toBe(true)
+    expect(wrapper.findComponent({ name: 'router-link' }).find('a').attributes('href')).equals(routeStub)
+  })
+
+  it('renders bookmark button', () => {
+    const wrapper = mount(BikeCard, { props: { data: mockedBike, fluid: true }, global: { plugins: [router] } })
+
+    expect(wrapper.findComponent(BikeBookmark).exists()).toBe(true)
+  })
+
+  it('renders price correctly', () => {
+    const wrapper = mount(BikeCard, { props: { data: mockedBike, fluid: true }, global: { plugins: [router] } })
+    const bikePriceStub = mount(BikePrice, {
+      props: { price: mockedBike.rate, currency: CurrencyCode.EUR, rate: 'daily' }
+    })
+
+    expect(wrapper.findComponent(BikePrice).exists()).toBe(true)
+    expect(wrapper.findComponent(BikePrice).text()).equal(bikePriceStub.text())
+  })
+
+  it('displays fluid layout', () => {
+    const wrapper = mount(BikeCard, { props: { data: mockedBike, fluid: true }, global: { plugins: [router] } })
+
+    expect(wrapper.classes()).includes('bike-card--fluid')
+  })
+})
